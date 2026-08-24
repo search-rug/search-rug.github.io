3bd23dbedf334af8f6855a5034db1771436fd391
Daniel Feitosa <d.feitosa@rug.nl>
2026-08-24 14:58:57 +0200

    Lazy-load LinkedIn embeds and cap initial visible count with a show-more button

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -9,3 +9,14 @@ menuTrigger.onclick = function() {
     menuContainer.classList.toggle('collapse');
     body.classList.toggle('overflow-hidden');
 }
+
+/*
+ * Reveal additional LinkedIn embeds on the news page (only present there)
+ */
+var showMoreLinkedinBtn = document.querySelector('#show-more-linkedin');
+if (showMoreLinkedinBtn) {
+    showMoreLinkedinBtn.onclick = function() {
+        document.querySelector('#more-linkedin-posts').classList.remove('hidden');
+        showMoreLinkedinBtn.classList.add('hidden');
+    }
+}
